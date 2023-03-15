456e1d8e254dbd3deff7d1d67d1a36eca617ee6e
Karlobyo <carlo.barbini90@gmail.com>
2023-03-15 19:21:46 +0100

    cloud_app

--- leukemic_det/webinterface/app_cloud.py
+++ leukemic_det/webinterface/app_cloud.py
@@ -1,15 +1,23 @@
-# from fastapi import FastAPI
-import streamlit as st
-#import numpy as np
-from PIL import Image
-import base64
-#import time
-#import os
-#import requests
-#import wavfile
-#import pretty_midi
-#import io
 
+import cv2 as cv
+import streamlit as st
+import numpy as np
+import base64
+#from leukemic_det.ml_logic.data import load_test_img_prelim
+from fastapi import FastAPI
+from google.cloud import storage
+import tensorflow
+from params import *
+
+
+# Create a client object using the credentials file
+client = storage.Client()
+bucket = client.bucket('leukemic-1')
+
+
+app = FastAPI()
+app.state.model = tensorflow.keras.models.load_model(
+            '/Users/carlobarbini/code/Karlobyo/leukemic_cell_detective_project/leukemic_det/webinterface/cnn_base_simple')
 
 if 'button_clicked' not in st.session_state:
     st.session_state.button_clicked = False
@@ -34,15 +42,6 @@ def add_bg_from_local(image_file):
 
 
 
-# app = FastAPI()
-
-# # Define a root `/` endpoint
-# @app.get('/')
-# def index():
-#     return {'ok': True}
-
-# @app.get('/electronify')
-
 st.set_page_config(layout='wide')
 
 CSS = """
@@ -60,6 +59,99 @@ st.write(f'<style>{CSS}</style>', unsafe_allow_html=True)
 st.title('Leukemic Cell Detective')
 
 
-st.markdown('### *Detecting healthy vs malignant cells from white blood cells microscopic images*')
+st.markdown('### *Detecting healthy vs malignant cells from human white blood cells microscopic images*')
 
-#add_bg_from_local('images/bg_app.png')
+st.markdown('Original dataset: https://wiki.cancerimagingarchive.net/pages/viewpage.action?pageId=52758223')
+
+st.markdown('Dataset publication: Gupta, A., & Gupta, R. (2019). ALL Challenge dataset of ISBI 2019 [Data set] The Cancer Imaging Archive https://doi.org/10.7937/tcia.2019.dc64i46r')
+
+st.markdown('This is a convolution neural network deep learning app meant to deliver real-time predictions classifiying human white blood cells microscopic images as healthy or malignant (acute lymphoblastic leukaemia)')
+
+if 'button_clicked' not in st.session_state:
+    st.session_state.button_clicked = False
+def callback():
+    st.session_state.button_clicked = True
+
+
+image_numbers = [int for int in range(1,1801)]
+# create a multi-select widget for selecting image numbers
+selected_image_number = st.multiselect('Please select an image (1800 samples available):', image_numbers)
+
+
+
+def load_test_img_prelim(img_sample: int): # returns unlabelled images from GCS bucket leukemic-1
+    
+    test_folder = bucket.blob("C-NMC_Leukemia/testing_data/C-NMC_test_prelim_phase_data")
+    test_image_paths = []
+    for blob in bucket.list_blobs(prefix=test_folder.name):
+        image_path = blob.name
+        test_image_paths.append(image_path)
+    
+    
+        
+    blob = bucket.blob(test_image_paths[img_sample])
+    image_bytes = blob.download_as_bytes()
+    nparr = np.frombuffer(image_bytes, np.uint8)
+    test_img = cv.imdecode(nparr, cv.IMREAD_COLOR)
+    
+    
+
+    s = np.resize((test_img), (450, 450, 3))
+    resized_test_img = np.array(s)
+
+    return resized_test_img
+
+
+
+
+
+@app.get("/show_img")
+def show_img_prelim(img_sample : int):
+
+    test_folder = bucket.blob("C-NMC_Leukemia/testing_data/C-NMC_test_prelim_phase_data")
+    test_image_paths = []
+    for blob in bucket.list_blobs(prefix=test_folder.name):
+        image_path = blob.name
+        test_image_paths.append(image_path)
+    
+    
+        
+    blob = bucket.blob(test_image_paths[img_sample])
+    image_bytes = blob.download_as_bytes()
+    nparr = np.frombuffer(image_bytes, np.uint8)
+    test_img = cv.imdecode(nparr, cv.IMREAD_COLOR)
+    
+    return test_img
+
+for i in selected_image_number:
+    st.image(show_img_prelim(i), width=200, caption=f'Human white blood cell #{i}')
+
+
+    @app.get("/predict")
+    def predict(img_sample : int):
+        """
+        Make a single image prediction
+        Assumes `img_sample' is provided as an integer index by the user
+        """
+        
+        X_pred = load_test_img_prelim(img_sample)
+        
+        model = app.state.model
+        assert model is not None
+        
+        X_pred = np.expand_dims(X_pred, 0)   
+        y_pred = model.predict(np.array(X_pred))
+        
+        y_pred = (y_pred > 0.5).astype(int)
+        
+        return y_pred
+    
+    
+    
+    predicted_class = predict(i)
+    if predicted_class == 0:
+        st.write('healthy')
+    else:
+        st.write('malignant')
+    
+    
