e4897aaeb71c80c63fcb3c77584b9043209d239d
Karlobyo <carlo.barbini90@gmail.com>
2023-03-23 09:26:06 +0100

    notebooks and apps cleanup

--- leukemic_det/webinterface/app_local.py
+++ leukemic_det/webinterface/app_local.py
@@ -6,15 +6,14 @@ import base64
 from fastapi import FastAPI
 from google.cloud import storage
 import tensorflow
-from params import *
 
 # Create a client object using the credentials file
 client = storage.Client()
-bucket = client.bucket(BUCKET_NAME)
+bucket = client.bucket('leukemic-1')
 
 app = FastAPI()
 app.state.model = tensorflow.keras.models.load_model(
-            '/Users/carlobarbini/code/Karlobyo/leukemic_cell_detective_project/leukemic_det/webinterface/cnn_base_simple')
+            'cnn_base_simple')
 model = app.state.model
 
 # if 'button_clicked' not in st.session_state:
@@ -69,11 +68,11 @@ st.markdown('')
 
 st.markdown('Original dataset: https://wiki.cancerimagingarchive.net/pages/viewpage.action?pageId=52758223')
 
-st.markdown('Dataset publication: Gupta, A., & Gupta, R. (2019). ALL Challenge dataset of ISBI 2019 [Data set] The Cancer Imaging Archive https://doi.org/10.7937/tcia.2019.dc64i46r')
+st.markdown('Dataset publication: Gupta, A., & Gupta, R. (2019). ALL Challenge dataset of ISBI 2019 [Data set]   \nThe Cancer Imaging Archive https://doi.org/10.7937/tcia.2019.dc64i46r')
 
 st.markdown('')
 
-st.markdown('This is a research preview of a convolution neural network deep learning app meant to deliver real-time predictions classifiying human white blood cells  \nmicroscopic images as healthy or malignant (acute lymphoblastic leukaemia)')
+st.markdown('This is a research preview of a convolution neural network deep learning app meant to deliver real-time predictions classifiying   \nhuman white blood cells microscopic images as healthy or malignant (acute lymphoblastic leukaemia)')
 
 st.markdown('***')
 
@@ -208,4 +207,4 @@ st.markdown('')
 
 st.markdown('')
     
-st.markdown('-In the best case your image should show an individual cell well defined from a black background-')
+st.markdown('-The model works best if your image shows an individual white blood cell well defined from a black background-')
