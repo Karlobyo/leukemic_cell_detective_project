bcaf38d23b5b5866fba921365c7b4aebd0ecc33d
Carlo Barbini <carlo.barbini90@gmail.com>
2024-03-14 10:06:58 +0100

    fix

--- leukemic_det/params.py
+++ leukemic_det/params.py
@@ -1,7 +1,4 @@
 import os
-import numpy as np
-from google.cloud import storage
-
 
 ##################  VARIABLES  ##################
 
@@ -9,7 +6,7 @@ URL=os.environ.get("URL")
 BATCH_SIZE=os.environ.get("BATCH_SIZE")
 DATA_SIZE = int(os.environ.get("DATA_SIZE"))
 CHUNK_SIZE = os.environ.get("CHUNK_SIZE")
-MODEL_TARGET = os.environ.get("MODEL_TARGET")
+
 MODEL=os.environ.get("MODEL")
 GCP_PROJECT = os.environ.get("GCP_PROJECT")
 GCP_REGION = os.environ.get("GCP_REGION")
@@ -17,11 +14,7 @@ BQ_DATASET = os.environ.get("BQ_DATASET")
 BQ_REGION = os.environ.get("BQ_REGION")
 BUCKET_NAME = os.environ.get("BUCKET_NAME")
 INSTANCE = os.environ.get("INSTANCE")
-MLFLOW_TRACKING_URI = os.environ.get("MLFLOW_TRACKING_URI")
-MLFLOW_EXPERIMENT = os.environ.get("MLFLOW_EXPERIMENT")
-MLFLOW_MODEL_NAME = os.environ.get("MLFLOW_MODEL_NAME")
-PREFECT_FLOW_NAME = os.environ.get("PREFECT_FLOW_NAME")
-PREFECT_LOG_LEVEL = os.environ.get("PREFECT_LOG_LEVEL")
+
 
 GCR_IMAGE = os.environ.get("GCR_IMAGE")
 GCR_REGION = os.environ.get("GCR_REGION")
