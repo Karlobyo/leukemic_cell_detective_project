c0fe67245d3b8e3e553841a650a7f9ae48c1ed22
carlobarbini <carlo.barbini90@gmail.com>
2024-08-29 15:47:41 +0200

    packaging

--- leukemic_det/webinterface/pages/1_Leuk_Cell_Det.py
+++ leukemic_det/webinterface/pages/1_Leuk_Cell_Det.py
@@ -85,24 +85,6 @@ def predict(X_pred):
 
 
 
-# add bg image
-# @st.cache_data
-# def add_bg_from_local(image_file):
-#     with open(image_file, "rb") as image_file:
-#         encoded_string = base64.b64encode(image_file.read())
-#     st.markdown(
-#     f"""
-#     <style>
-#     .stApp {{
-#         background-image: url(data:image/{"jpg"};base64,{encoded_string.decode()});
-#         background-size: cover
-#     }}
-#     </style>
-#     """,
-#     unsafe_allow_html=True
-#     )
-
-
 
 CSS = """
 h1 {
