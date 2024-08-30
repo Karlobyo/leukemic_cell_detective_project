e0a22d78223c878de7301d361292d77f7a0b1112
carlobarbini <carlo.barbini90@gmail.com>
2024-08-30 15:25:44 +0200

    params

--- leukemic_det/params.py
+++ leukemic_det/params.py
@@ -1,9 +1,9 @@
 import os
 
-MODEL_TARGET = "local"
+MODEL_TARGET = "gcs"
 
 BUCKET_NAME =   "leukemic-1"
 
 LOCAL_REGISTRY_PATH =  os.path.join("leukemic_det", "webinterface", "model_dir")
 
-UI = "local"
+UI = "cloud"
