0623ce90ea1facc92506f26f9503828a9160dc4c
carlobarbini <carlo.barbini90@gmail.com>
2024-08-30 09:24:46 +0200

    packaging

--- leukemic_det/webinterface/pages/1_Leuk_Cell_Det.py
+++ leukemic_det/webinterface/pages/1_Leuk_Cell_Det.py
@@ -3,7 +3,6 @@ import streamlit as st
 import cv2 as cv
 
 import numpy as np
-import tensorflow as tf
 
 from google.cloud import storage
 from google.oauth2 import service_account
@@ -11,80 +10,25 @@ from google.oauth2 import service_account
 from bg_loader import add_bg_from_local
 
 from leukemic_det.ml_logic.data_classification import show_img, predict
+from leukemic_det.ml_logic.registry import load_model
 
-
-# Retrieve the secrets
+# Retrieve the gcp account secrets
 service_account_info = st.secrets["gcp_service_account"]
 
-# Create credentials object from the secrets
+# Create credentials and bucket object from the secrets
 credentials = service_account.Credentials.from_service_account_info(service_account_info)
-#credentials = service_account_info["private_key"]
-
-#credentials, project = auth.default()
-
 bucket = st.secrets["bucket"]
 
 # Initialize the client with the credentials
-
 client = storage.Client(project=service_account_info["project_id"], credentials=credentials)
-
 bucket = client.bucket(bucket)
 
 
 st.set_page_config(layout='wide')
 
 
-# model for classifying uploaded image (when there is no active Google Cloud Run URL)
-@st.cache_resource
-def load_model():
-    return tf.keras.models.load_model(
-    "leukemic_det/webinterface/model_dir/20240312-114546.h5")
-
+# local model for classifying uploaded image (when there is no active Google Cloud Run URL)
 model = load_model()
-
-### functions ###
-
-
-# getting paths of test images
-# @st.cache_data(show_spinner=False)
-# def get_imgs_paths():
-#     test_folder = bucket.blob("C-NMC_Leukemia/testing_data/C-NMC_test_prelim_phase_data")
-#     imgs_paths = []
-#     for blob in bucket.list_blobs(prefix=test_folder.name):
-#         blob_image_paths = blob.name
-#         imgs_paths.append(blob_image_paths)
-#     return imgs_paths
-
-
-# display chosen image
-# @st.cache_data(show_spinner=False)
-# def show_img(img_sample : int):
-
-#     imgs_paths = get_imgs_paths()
-
-#     # decoding the img path
-#     img_path = bucket.blob(imgs_paths[img_sample])
-#     image_bytes = img_path.download_as_bytes()
-#     nparr = np.frombuffer(image_bytes, np.uint8)
-#     chosen_img = cv.imdecode(nparr, cv.IMREAD_COLOR)
-
-#     return chosen_img
-
-
-
-# classify image
-# def predict(X_pred):
-#     """
-#     Makes a single image prediction
-#     Returns 0 for healthy and 1 for malignant
-#     """
-#     resized = np.resize((X_pred), (450, 450, 3))
-#     X_pred = np.expand_dims(resized, 0)
-#     y_pred = model.predict(X_pred)
-#     predicted_class = (y_pred > 0.5).astype(int)
-#     return predicted_class
-
-
 
 
 CSS = """
