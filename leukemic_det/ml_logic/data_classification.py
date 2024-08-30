5795102c9768df6299fe664d7774166877e6aaae
carlobarbini <carlo.barbini90@gmail.com>
2024-08-30 09:32:17 +0200

    packaging

--- leukemic_det/ml_logic/data_classification.py
+++ leukemic_det/ml_logic/data_classification.py
@@ -60,24 +60,24 @@ def show_img(img_sample : int):
 
 
 
-def show_img_prelim(img_sample : int):
+# def show_img_prelim(img_sample : int):
 
-    # getting bucket paths of test images
-    test_folder = bucket.blob("C-NMC_Leukemia/testing_data/C-NMC_test_prelim_phase_data")
-    test_image_paths = []
-    for blob in bucket.list_blobs(prefix=test_folder.name):
-        image_path = blob.name
-        test_image_paths.append(image_path)
+#     # getting bucket paths of test images
+#     test_folder = bucket.blob("C-NMC_Leukemia/testing_data/C-NMC_test_prelim_phase_data")
+#     test_image_paths = []
+#     for blob in bucket.list_blobs(prefix=test_folder.name):
+#         image_path = blob.name
+#         test_image_paths.append(image_path)
 
-    # deconding the imgs paths into images
-    test_imgs =[]
-    blob = bucket.blob(test_image_paths[img_sample])
-    image_bytes = blob.download_as_bytes()
-    nparr = np.frombuffer(image_bytes, np.uint8)
-    test_img = cv.imdecode(nparr, cv.IMREAD_COLOR)
-    test_imgs.append(test_img)
+#     # deconding the imgs paths into images
+#     test_imgs =[]
+#     blob = bucket.blob(test_image_paths[img_sample])
+#     image_bytes = blob.download_as_bytes()
+#     nparr = np.frombuffer(image_bytes, np.uint8)
+#     test_img = cv.imdecode(nparr, cv.IMREAD_COLOR)
+#     test_imgs.append(test_img)
 
-    return test_imgs
+#     return test_imgs
 
 
 def predict(X_pred):
