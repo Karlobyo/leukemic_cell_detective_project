b336dcabb82284889349e0ed21ac1f41572a2935
carlobarbini <carlo.barbini90@gmail.com>
2024-08-30 09:35:47 +0200

    packaging

--- leukemic_det/ml_logic/data_classification.py
+++ leukemic_det/ml_logic/data_classification.py
@@ -1,12 +1,11 @@
+import streamlit as st
 import numpy as np
 import cv2 as cv
-import tensorflow as tf
+
 from google.cloud import storage
-import streamlit as st
 from google.oauth2 import service_account
 
 from leukemic_det.ml_logic.registry import load_model
-
 
 
 # Create a client object using the credentials file
@@ -14,22 +13,12 @@ service_account_info = st.secrets["gcp_service_account"]
 
 # Create credentials object from the secrets
 credentials = service_account.Credentials.from_service_account_info(service_account_info)
-#credentials = service_account_info["private_key"]
-
-#credentials, project = auth.default()
-
 bucket = st.secrets["bucket"]
 
 # Initialize the client with the credentials
 
 client = storage.Client(project=service_account_info["project_id"], credentials=credentials)
-
 bucket = client.bucket(bucket)
-
-#model = tf.keras.models.load_model(
-#    "leukemic_det/webinterface/model_dir/20240312-114546.h5")
-
-model = load_model()
 
 
 @st.cache_data(show_spinner=False)
@@ -57,29 +46,6 @@ def show_img(img_sample : int):
     return chosen_img
 
 
-
-
-
-# def show_img_prelim(img_sample : int):
-
-#     # getting bucket paths of test images
-#     test_folder = bucket.blob("C-NMC_Leukemia/testing_data/C-NMC_test_prelim_phase_data")
-#     test_image_paths = []
-#     for blob in bucket.list_blobs(prefix=test_folder.name):
-#         image_path = blob.name
-#         test_image_paths.append(image_path)
-
-#     # deconding the imgs paths into images
-#     test_imgs =[]
-#     blob = bucket.blob(test_image_paths[img_sample])
-#     image_bytes = blob.download_as_bytes()
-#     nparr = np.frombuffer(image_bytes, np.uint8)
-#     test_img = cv.imdecode(nparr, cv.IMREAD_COLOR)
-#     test_imgs.append(test_img)
-
-#     return test_imgs
-
-
 def predict(X_pred):
     """
     Makes a single image prediction
@@ -89,6 +55,8 @@ def predict(X_pred):
     resized = np.resize((X_pred), (450, 450, 3))
 
     X_pred = np.expand_dims(resized, 0)
+
+    model = load_model()
 
     y_pred = model.predict(np.array(X_pred))
 
